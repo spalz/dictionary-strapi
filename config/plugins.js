e470a83720a1b5d0976da72513e8a05f19112277
Yurii Repin <spalz@mail.ru>
2022-09-24 20:48:16 +0600

    up

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: "slug",
+          references: "title",
+        },
+      },
+    },
+  },
+  "duplicate-button": true,
+});
