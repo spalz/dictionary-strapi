4573befecde6a6e834f8b45cbd33cc96a32fca5d
Yurii Repin <spalz@mail.ru>
2022-09-25 13:20:37 +0600

    fix database.js

--- config/production/database.js
+++ config/production/database.js
@@ -1,14 +1,17 @@
-module.exports = ({ env }) => ({
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        rejectUnauthorized: false,
       },
     },
     debug: false,
