4bf347d59e3dc10616d5c21d8aecb1b361c0af0b
Yurii Repin <spalz@mail.ru>
2022-10-03 13:17:46 +0600

    up

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: "https://admin.dictionary.dangercactus.io",
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
   },
